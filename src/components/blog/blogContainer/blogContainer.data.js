e5291dea2591927e734b160a9beea2f4766e0577
vandoan882000 <vandoan882000@gmail.com>
2022-04-07 09:57:39 +0700

    test

--- src/components/blog/blogContainer/blogContainer.data.js
+++ src/components/blog/blogContainer/blogContainer.data.js
@@ -38,4 +38,173 @@ const data = {
       lg: 4
     }
   },
+  product_box: [
+    {
+      type: "search"
+    },
+    {
+      type: "refine",
+      item: [
+        {
+          name: "Yellow"
+        },
+        {
+          name: "Hat"
+        },
+        {
+          name: "XS"
+        }
+      ]
+    },
+    {
+      type: "category",
+      item: [
+        {
+          name: "Áo"
+        },
+        {
+          name: "Váy"
+        },
+        {
+          name: "Quần"
+        },
+        {
+          name: "Bikini"
+        },
+        {
+          name: "Phụ kiện"
+        }
+      ]
+    },
+    {
+      type: "color",
+      item: [
+        {
+          name: "--1"
+        },
+        {
+          name: "--2"
+        },
+        {
+          name: "--2"
+        },
+        {
+          name: "--2"
+        },
+        {
+          name: "--2"
+        },
+        {
+          name: "--2"
+        },
+        {
+          name: "--2"
+        }
+      ]
+    },
+    {
+      type: "size",
+      item: [
+        {
+          name: "XS"
+        },
+        {
+          name: "S"
+        },
+        {
+          name: "M"
+        },
+        {
+          name: "L"
+        },
+        {
+          name: "XL"
+        }
+      ]
+    },
+    {
+      type: "brand",
+      item: [
+        {
+          name: "Gap"
+        },
+        {
+          name: "Levi"
+        },
+        {
+          name: "Polo"
+        },
+        {
+          name: "Zara"
+        },
+        {
+          name: "Gucci"
+        }
+      ]
+    },
+    {
+      type: "availability",
+      item: [
+        {
+          name: "In stock"
+        },
+        {
+          name: "Out of stosck"
+        }
+      ]
+    },
+    {
+      type: "tag",
+      item: [
+        {
+          name: "Design"
+        },
+        {
+          name: "Save"
+        },
+        {
+          name: "Simple"
+        },
+        {
+          name: "Stock"
+        },
+        {
+          name: "Hãy tìm key đi"
+        }
+        ,
+        {
+          name: "Item"
+        }
+        ,
+        {
+          name: "Iec"
+        }
+        ,
+        {
+          name: "Linh Tinh"
+        }
+      ]
+    },
+    {
+      type: "popular-post",
+      item: [
+        {
+          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          srcImg: "../../img/post1.png"
+        },
+        {
+          title: "Nisl natoque sed cursus purus. Pellentesque amet sollicitudin non ac adipiscing sed fac...",
+          srcImg: "../../img/post2.png"
+        },
+        {
+          title: "A arcu malesuada mauris consequat est cursus dignissim cras.",
+          srcImg: "../../img/post3.png"
+        }
+      ]
+    },
+    {
+      type: "newsletter"
+    }
+
+  ]
 };

2a2cb051c308cd4df8a3ef6316647c3a5fa8072a
vandoan882000 <vandoan882000@gmail.com>
2022-05-16 15:34:51 +0700

    fix data

---
+++ src/components/account/accountcon/accountContainer.data.js
@@ -0,0 +1,12 @@
+const data = {
+  general_settings: {
+    fixed: false,
+    background_color_enable: false,
+    color: "#E5E5E5",
+    background_image_enable: true,
+    image: "",
+    size: "cover",
+    overlay: "transparent",
+  },
+
+};
--- src/components/cart/carts/sectionCart.data.js
+++ src/components/cart/carts/sectionCart.data.js
@@ -12,29 +12,5 @@ const data = {
     text: "Cart",
     design: "",
   },
-  sidebar: {
-    banner: {
-      img: "img/banner-1.png",
-      sub_title: "TREATS & GROOMING",
-      title: "Spoil your true love",
-    },
-  },
-  content: {
-    collection: "402146787549",
-    review_app: {
-      enable: true,
-      app: "product_reviews", // "product_reviews" or "rivyo_reviews" or "loox_reviews""
-    },
-    sale: {
-      enable: true,
-      text: "Sale",
-      design: "",
-    },
-    column: {
-      xs: 1,
-      sm: 2,
-      md: 3,
-      lg: 4
-    }
-  },
+
 };

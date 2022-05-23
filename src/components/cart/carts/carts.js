9203362c330278879808529e4160f26b65c59538
vandoan882000 <vandoan882000@gmail.com>
2022-05-23 10:45:05 +0700

    fix cart

--- src/components/cart/carts/carts.js
+++ src/components/cart/carts/carts.js
@@ -6,6 +6,7 @@ const { message } = veda.plugins;
 const { html, render } = veda.utils.csr;
 const PREFIX = "yasmina";
 
+
 function onChangeQuantity(event, id) {
   const currentEl = event.target;
   const quantity = currentEl.value;
@@ -28,12 +29,19 @@ function handleClickDecrement(event, id) {
 function handleClickIncrement(event, id) {
   const currentEl = event.target;
   const quantityEl = currentEl.closest('.veda-counter').querySelector('.veda-counter__input');
-  quantityEl.value = parseInt(quantityEl.value) + 1;
-  if (quantityEl.value == 0) {
-    veda.plugins.cart.removeCart(id);
-  } else {
-    veda.plugins.cart.updateCart(id, quantityEl.value);
-  }
+  // quantityEl.setAttribute("value", parseInt(quantityEl.value) + 1);
+  quantityEl.value = Number(quantityEl.value) + 1;
+
+  // quantityEl.value = parseInt(quantityEl.value) + 1;
+  // if (quantityEl.value == 0) {
+  //   veda.plugins.cart.removeCart(id);
+  // } else {
+  //   veda.plugins.cart.updateCart(id, quantityEl.value);
+  // }
+
+}
+function onClickTest(event) {
+  console.log(event);
 }
 if(!!container) {
 
@@ -43,8 +51,8 @@ if(!!container) {
       return html`
         <tr class="card-cart fz:15px bd:none bdb:1px_solid_color-gray2">
           <td class="card-cart__content bd:none pt:30px pb:30px d:flex pl:0px!">
-              <a href="#" class="veda-image-cover d:block w:140px bd:none! h:132px" css="--aspect-ratio: 4/4">
-                <img src=${product.image} alt="product-card"></img>
+            <a href="#" class="veda-image-cover d:block w:140px bd:none! h:132px" css="--aspect-ratio: 4/4">
+              <img src=${product.featured_image.src} alt="product-card"></img>
               </a>
               <div class="card-cart__name pl:20px c:color-primary">
                 <div class="card-cart__title c:color-dark mb:10px fw:500 fz:15px">${product.title}</div>
@@ -56,11 +64,11 @@ if(!!container) {
               <div class="c:color-dark fw:400 fz:20px">$${product.price}.00</div>
             </td>
             <td class=" bd:none va:middle!">
-              <div data-id="${product.id}" class="veda-counter d:flex w:108px bd:1px_solid_color-gray3" data-options="{ value: ${product.quantity} }">
+              <div data-id="${product.id}" class="veda-counter d:flex w:108px bd:1px_solid_color-gray3">
                 <div class="veda-counter__decrement c:color-dark w:30px h:40px bd:none o:none bgc:transparent d:flex ai:center jc:flex-start cur:pointer bgc:transparent!|h c:color-dark!|h ta:center" onClick=${debounce(event => handleClickDecrement(event, product.id))}>
                   <i class="fal fa-minus fz:13px w:100% h:100% lh:40px"></i>
                 </div>
-                <input class="veda-counter__input w:48px! h:40px p:0! bd:none! o:none ta:center fz:15px fw:500 c:color-gray9" type="number" data-button="disabled" onInPut=${debounce(event => onChangeQuantity(event, product.id))} value=${product.quantity}/>
+                <input class="veda-counter__input w:48px! h:40px p:0! bd:none! o:none ta:center fz:15px fw:500 c:color-gray9" type="number" data-button="disabled" onChange=${() => console.log("change")} onInPut=${debounce(event => onChangeQuantity(event, product.id))} value=${product.quantity} />
                 <div class="veda-counter__increment c:color-dark w:30px h:40px bd:none o:none bgc:transparent d:flex ai:center jc:flex-end bgc:transparent!|h cur:pointer c:color-dark!|h ta:center" onClick=${debounce(event => handleClickIncrement(event, product.id))}>
                   <i class="fal fa-plus fz:13px w:100% h:100% lh:40px"></i>
                 </div>
